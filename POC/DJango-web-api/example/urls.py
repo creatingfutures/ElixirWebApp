bb9e331fedbc1170cec9bc9011d708fa78446ee0
Abhishek Thomas <43369973+abhishekthomas17@users.noreply.github.com>
2020-04-04 18:58:10 +0530

    Add files via upload

---
+++ POC/DJango-web-api/example/urls.py
@@ -0,0 +1,36 @@
+"""example URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from user import views
+
+from rest_framework import routers
+
+# router=routers.DefaultRouter()
+# router.register(r'emp',views.employeeList)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('employee/',views.employeeList.as_view(),name='employee'),
+    path('employee1/',views.employee_view,name='employee1'),
+    path('employee2/<id>',views.employee_update,name='employee2'),
+    path('employee3',views.employee_create,name='employee3'),
+    #path("employee/",include(router.urls)),
+
+
+]
