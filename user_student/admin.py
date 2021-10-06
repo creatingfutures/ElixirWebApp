7abf519fb4ad9993ad191e577a46720bbfa61a2d
prasidha1 <32433336+prasidha1@users.noreply.github.com>
2021-10-06 13:56:26 +0530

    Update admin.py

--- user_student/admin.py
+++ user_student/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import scores,skillmaster,skills
+from .models import scores,skillmaster,skills, Stream, Job, Exam, Course, Prereq_exam
 
 #admin.site.register(student_status)
 admin.site.register(scores)
 admin.site.register(skills)
 admin.site.register(skillmaster)
+admin.site.register(Stream)
+admin.site.register(Job)
+admin.site.register(Exam)
+admin.site.register(Prereq_exam)
+admin.site.register(Course)
