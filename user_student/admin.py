0ba37f50322f38aad1d18fd579ed19705426ea05
prasidha1 <32433336+prasidha1@users.noreply.github.com>
2021-11-17 22:28:14 +0530

    Update admin.py

--- user_student/admin.py
+++ user_student/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import scores,skillmaster,skills, Stream, Job, Exam, Course, Prereq_exam
 
 #admin.site.register(student_status)
-admin.site.register(scores)
 admin.site.register(skills)
 admin.site.register(skillmaster)
 admin.site.register(Stream)
@@ -11,3 +10,7 @@ admin.site.register(Job)
 admin.site.register(Exam)
 admin.site.register(Prereq_exam)
 admin.site.register(Course)
+@admin.register(scores)
+class MemberAdmin(ImportExportModelAdmin):
+    list_display = ('id','student_id','batch_id','level_id','date_time','user_score','total_score','question_content_id','assessment_type_id','updated_date')
+    pass
